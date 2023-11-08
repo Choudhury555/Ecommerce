75879f71451e17e22796298b79228d947eaddedd
Choudhury555 <choudhurypanda143@gmail.com>
2023-11-08 07:53:19 +0530

    Temporary change

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({children}) => {
   return (
     <div className='layout'>
       <Head>
-        <title>Abhisek's Store</title>
+        <title>Abhisek Store</title>
       </Head>
 
       <header>
